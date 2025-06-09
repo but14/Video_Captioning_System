49f40220744938365ed12f470ef2b3ff75f390c5
Longngoo <100961248+but14@users.noreply.github.com>
2025-06-10 03:45:54 +0700

    PORT

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def predict_caption():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
